890d09a4e0941ed018c70acf9a55d6a2dd802721
Ibarron <juanpabloibarron@ciencias.unam.mx>
2019-02-13 13:39:48 -0600

    Prueba

---
+++ clase1.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3.6
+
+bool = [False, True]
+print ('x\ty\tx or y')
+#Tabla de verdad de 'or'
+for x in bool:
+    for y in bool:
+        print (x,' ', y, ' ' , x or y)
+print ()
+
+#Tabla de verdad de 'and'
+print ('x\ty\tx and y')
+for x in bool:
+        for y in bool:
+            print (x, ' ',y,' ', x and y)
+print ()
+
+#Tabla de verdad de 'not'
+print ('x\t falso x')
+for x in bool:
+    for y in bool:
+        print (x,' ', not x)
+print ()
+
+
+
+
+
+
+
+
